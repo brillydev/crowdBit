487e0f0b5601b2a71b0a5c091378dbf73bf3e2af
Prakrit Duangsutha <prakrit_duangsutha@outlook.com>
2018-10-13 01:11:26 -0400

    Added cloud storage initialization

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ var serviceAccount = require('firebase-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://datalyzer-63388.appspot.com",
   databaseURL: "https://datalyzer-63388.firebaseio.com"
 });
 
 var db = admin.firestore();
+var bucket = admin.storage().bucket();
 
 var app = express();
 /* GET home page. */
