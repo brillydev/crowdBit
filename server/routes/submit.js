45d96ba258df1c5324c0037953ed9696a58d07d9
Prakrit Duangsutha <prakrit_duangsutha@outlook.com>
2018-10-13 11:42:54 -0400

    added clarifai in submit.js

--- server/routes/submit.js
+++ server/routes/submit.js
@@ -15,7 +15,33 @@ router.post('/:name', upload.array('objects'), function(req, res, next) {
     let uri = req.params.name.toString();
 
     for (let obj of req.files) {
-        obj.path
+        let base64str = helper.base64_encode(obj);
+        clarifai.clarifaiApp.models.predict(Clarifai.GENERAL_MODEL, {
+            base64: base64str
+        })
+        .then(res => {
+            res = JSON.parse(res);
+            
+
+            for (let category of res.data.concepts) {
+                if (uri == category.name) {
+
+                    // upload to GCS
+                    await firebase.bucket('objects').upload(obj.path, {
+                        gzip: true
+                    });
+
+                    // rename file in GCS to appear "in folder"
+                    await firebase.bucket('objects')
+                                    .file(obj.filename)
+                                    .move(category.name + '/' + obj.filename);
+                } else {
+                    // TODO: if we have time, redirect to user for verification
+                }
+            }
+        },
+            (err) => { console.log(err.toString()); }
+        )
     }
     
     let content = await exec("../gsutil/gsutil -m cp -r gs://" + 
