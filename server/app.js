80aae6bc39e0ec46bb9bef7b962600fcad424d2a
Prakrit Duangsutha <prakrit_duangsutha@outlook.com>
2018-10-13 03:25:02 -0400

    Moved firebase initialization to app.js

--- server/app.js
+++ server/app.js
@@ -9,6 +9,19 @@ var usersRouter = require('./routes/users');
 
 var app = express()
 
+// firebase
+var admin = require('firebase-admin');
+var serviceAccount = require('./firebase-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://datalyzer-63388.appspot.com",
+  databaseURL: "https://datalyzer-63388.firebaseio.com"
+});
+
+var db = admin.firestore();
+var bucket = admin.storage().bucket();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
