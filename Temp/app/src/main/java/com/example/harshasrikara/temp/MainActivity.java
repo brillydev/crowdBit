bd57e0e730d792613ff700c8b0271b182fcced3e
harsha <harshasrikara@gmail.com>
2018-10-14 07:25:40 -0400

    completed wireframe for app

--- Temp/app/src/main/java/com/example/harshasrikara/temp/MainActivity.java
+++ Temp/app/src/main/java/com/example/harshasrikara/temp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.harshasrikara.temp;
 
+import android.app.DownloadManager;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.android.gms.auth.api.Auth;
@@ -27,8 +30,24 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 //import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
     SignInButton signInButton;
@@ -39,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN =9001;
     public final static String CURRENT_COUNT_KEY = "CurrentCount";
+    String send;
+
 
 
     @Override
@@ -98,6 +119,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             // Signed in successfully, show authenticated UI.
             statusTextView.setText("congrats, you're logged in");
             setContentView(R.layout.sender_request);
+            EditText text = findViewById(R.id.input);
+
+            send = text.getText().toString();
 
  //           if(statusTextView.getText().toString()=="congrats, you're logged in") {
  //               Intent intent = new Intent(this,RequestSender.class);
@@ -128,11 +152,61 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             }
     public void sendRequest(View view)
     {
-            //do nothing
+        getRequest();
+        //do nothing
+        //use the string variable "send"
+        OutputStream out = null;
+        try {
+            URL url = new URL("http://82df21e7.ngrok.io/task/create/"+send);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            writer.write(send);
+            writer.flush();
+            writer.close();
+            out.close();
+
+            urlConnection.connect();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    //Get Request
+
+    public void getRequest()
+    {
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text , duration);
+        toast.show();
+
+        TextView tx = findViewById(R.id.result);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://82df21e7.ngrok.io/task/create"+send;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
     }
 
-            
-        }//);
+//);
 
 
 
