1d71b16f8e459dd5dc578f994d5f89ee2f87c128
Prakrit Duangsutha <prakrit_duangsutha@outlook.com>
2018-10-14 10:34:48 -0400

    Submit and Download now works

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Datalyzer'});
+  res.render('index', { title: 'crowdBit'});
 });
 
 router.get('/getNextTask', function (req, res, next) {
